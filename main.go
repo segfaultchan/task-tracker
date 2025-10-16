94992a7cfe14638fd16ed6f1beba59846d5a3b6a
segfault <segfault@example.com>
2025-10-16 22:51:56 +0300

    input function

--- main.go
+++ main.go
@@ -3,18 +3,37 @@ package main
 import (
 	"fmt"
 	"time"
+	"errors"
 )
 
-
+const (
+	PROMPT = "tasks-$ "
+)
 
 type Task struct {
 	id int
 	desc string
-	is_done bool
-	created_at time.Time
-	updated_at time.Time
+	isDone bool
+	createdAt time.Time
+	updatedAt time.Time
+}
+
+func smartInput(prompt ...string) (string, error) {
+	if len(prompt) == 0 {
+		fmt.Print("input-$ ")
+	} else {
+	fmt.Print(prompt[0])
+	}
+	var input string
+	_, e := fmt.Scanln(&input)
+	if e != nil {
+		e = errors.New("input error")
+		return "", e
+	}
+	return input, nil
 }
 
 func main() {
-	
+	input, _ := smartInput(PROMPT)
+	fmt.Println("input:", input)
 }
